5151d1e78b8f6e02a227674b00a357d2f7cf84fa
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-10-24 16:07:44 +0200

    add loop example

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"time"
 
 	"github.com/bjartek/overflow"
+	"github.com/samber/lo"
 	"go.uber.org/zap"
 )
 
 func main() {
-	start := time.Now()
 	o := overflow.Overflow(overflow.WithNetwork("mainnet"))
 
 	logger, _ := zap.NewDevelopment()
@@ -18,18 +18,24 @@ func main() {
 	ctx := context.Background()
 
 	// latest, _ := o.GetLatestBlock(ctx)
-	height := uint64(63723171)
+	height := uint64(63800000)
 
-	block, err := o.GetBlockAtHeight(ctx, height)
-	if err != nil {
-		logger.Fatal("did not get block", zap.Error(err))
+	rng := lo.RangeFrom(height, 10000)
+
+	for _, h := range rng {
+		start := time.Now()
+		block, err := o.GetBlockAtHeight(ctx, h)
+		if err != nil {
+			logger.Fatal("did not get block", zap.Error(err))
+		}
+
+		tx, txR, err := o.Flowkit.GetTransactionsByBlockID(ctx, block.ID)
+		if err != nil {
+			logger.Fatal("did not get tx results", zap.Error(err))
+		}
+
+		taken := time.Since(start)
+		logger.Info("get block", zap.Uint64("height", h), zap.Int("tx", len(tx)), zap.Int("txr", len(txR)), zap.Duration("time", taken))
 	}
-
-	tx, txR, err := o.Flowkit.GetTransactionsByBlockID(ctx, block.ID)
-	if err != nil {
-		logger.Fatal("did not get tx results", zap.Error(err))
-	}
-
-	taken := time.Since(start)
-	logger.Info("done", zap.Int("tx", len(tx)), zap.Int("txr", len(txR)), zap.Duration("time", taken))
+	logger.Info("done")
 }
